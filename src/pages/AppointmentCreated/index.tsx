0d2ab514b947611a3cdedab0582ffb5778c2c814
Gustavo <gustavoplensack@gmail.com>
2021-06-02 08:49:01 -0300

    Add AppointmentCreated screen

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,9 +1,58 @@
-import React from 'react';
-import { View } from 'react-native';
+/* eslint-disable import/no-duplicates */
+import React, { useCallback, useMemo } from 'react';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { format } from 'date-fns';
+
+import ptBR from 'date-fns/locale/pt-BR';
+
+import {
+  Container,
+  Title,
+  Description,
+  OkButton,
+  OkButtonText,
+} from './styles';
+
+interface IRouteParams {
+  date: number;
+}
 
 const AppointmentCreated: React.FC = () => {
+  const { reset } = useNavigation();
+
+  const { params } = useRoute();
+
+  const { date } = params as IRouteParams;
+
+  const formattedDate = useMemo(() => {
+    return format(date, "EEEE', dia' dd 'de' MMMM 'de' yyyy 'às' HH':00h'", {
+      locale: ptBR,
+    });
+  }, [date]);
+
+  const handleOkPressed = useCallback(() => {
+    reset({
+      routes: [
+        {
+          name: 'Dashboard',
+        },
+      ],
+      index: 0,
+    });
+  }, [reset]);
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }} />
+    <Container>
+      <Icon name="check" size={80} color="#04d361" />
+
+      <Title>Agendamento concluído</Title>
+      <Description>{formattedDate}</Description>
+
+      <OkButton onPress={handleOkPressed}>
+        <OkButtonText>OK</OkButtonText>
+      </OkButton>
+    </Container>
   );
 };
 
